39b6846bb3c6d739bf80a143324d8ba618801062
Prabhdeep Singh <prabhdeepsingh@Prabhdeeps-MacBook-Pro.local>
2024-11-17 15:28:13 -0500

    first commit

---
+++ src/main/java/com/acc/laptop_recommendation/LaptopRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.acc.laptop_recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopRecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acc/laptop_recommendation/LaptopRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acc.laptop_recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
